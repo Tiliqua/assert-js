7b3c6f1dee62e3ad70d147dce84e4341d1400379
Norbert Orzechowicz <norbert@orzechowicz.pl>
2016-05-07 19:38:56 +0200

    Added missing assertions

--- src/JSAssert/Assert.js
+++ src/JSAssert/Assert.js
@@ -323,6 +323,9 @@ export default class Assert
      */
     static url(urlValue, message = "")
     {
+        this.string(urlValue);
+        this.string(message, "Custom error message passed to Assert.jsonString needs to be a valid string.");
+
         let regexp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
         if (!regexp.test(urlValue)) {
@@ -332,10 +335,13 @@ export default class Assert
 
     /**
      * @param {string} uuidValue
-     * @param {string} [$message]
+     * @param {string} [message]
      */
     static uuid(uuidValue, message = "")
     {
+        this.string(uuidValue);
+        this.string(message, "Custom error message passed to Assert.jsonString needs to be a valid string.");
+
         let regexp = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
         if (!regexp.test(uuidValue)) {
